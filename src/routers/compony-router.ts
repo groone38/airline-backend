9207bcf4a9dfc266659e03180a8975b8423c2c12
groone38 <groone38@yandex.ru>
2023-06-01 23:57:48 +0300

    create user route post

--- src/routers/compony-router.ts
+++ src/routers/compony-router.ts
@@ -28,9 +28,3 @@ componyRouter.get("/", authenticateJWT, async (req: Request, res: Response) => {
   const company = await getCompany();
   res.status(200).json(company);
 });
-
-componyRouter.get("/:id/users", async (req: Request, res: Response) => {
-  const company = await getCompanyOne(req.params.id);
-  const users = await getUsersInCompany(company[0].id);
-  res.status(200).json(users);
-});
--- src/routers/users-router.ts
+++ src/routers/users-router.ts
@@ -1,10 +1,60 @@
 import { Request, Response, Router } from "express";
-import { getCompany, getUsers, getUsersInCompany } from "../database";
+import {
+  getCompany,
+  getCompanyOne,
+  getUsers,
+  getUsersInCompany,
+  registrUser,
+} from "../database";
 
 export const usersRouter = Router();
 const jwt = require("jsonwebtoken");
 
-usersRouter.get("/", async (req: Request, res: Response) => {
-  const users = await getUsers();
-  res.send(users);
+export const authenticateJWT = (req: any, res: Response, next: any) => {
+  const authHeader = req.headers.authorization;
+  console.log("qwe");
+  if (authHeader) {
+    // const token = authHeader.split(" ")[1];
+    const token = authHeader;
+
+    jwt.verify(token, process.env.JWT_SECRET, (err: any, user: any) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      req.user = user;
+
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+usersRouter.get(
+  "/:id/users",
+  authenticateJWT,
+  async (req: Request, res: Response) => {
+    const company = await getCompanyOne(req.params.id);
+    const users = await getUsersInCompany(company[0].id);
+    res.status(200).json(users);
+  }
+);
+
+usersRouter.post("/", authenticateJWT, async (req: Request, res: Response) => {
+  const user = await registrUser(req.body);
+  console.log(user);
+  if (user === "error") {
+    res.status(403).json({
+      message: "This user already exists",
+    });
+  } else if (user === "Not found Company!") {
+    res.status(404).json({
+      message: "Not found Company!",
+    });
+  } else {
+    res.status(200).json({
+      message: "Registr complite",
+    });
+  }
 });
